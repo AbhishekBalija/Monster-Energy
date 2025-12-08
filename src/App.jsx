2d82a6fcf0774b9ad1d718e032676a23c8e411d4
AbhishekBalija <abhiarsikere0001@gmail.com>
2025-12-09 00:12:50 +0530

    feat: Add new Fuel Section component with background video and `lucide-react` integration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import LoadingScreen from "./components/LoadingScreen";
 import Hero from "./components/Hero";
+import FuelSection from "./components/FuelSection";
 import Navbar from "./components/Navbar";
 import FlavorSection from "./components/FlavorSection";
 
@@ -9,6 +10,7 @@ function App() {
       <LoadingScreen />
       <Navbar />
       <Hero />
+      <FuelSection />
       <FlavorSection />
     </>
   );
