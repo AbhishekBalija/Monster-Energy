5ebf0379e81a128e1a934dd40388996eb0cc8c77
AbhishekBalija <abhiarsikere0001@gmail.com>
2025-12-25 17:47:39 +0530

    feat: Introduce interactive 3D lifestyle section with custom shader-based monoliths and supporting image assets.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import FuelSection from "./components/FuelSection";
 import Navbar from "./components/Navbar";
 import FlavorSection from "./components/FlavorSection";
+import LifestyleSection from "./components/LifestyleSection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <FuelSection />
+      <LifestyleSection />
       <FlavorSection />
     </>
   );
